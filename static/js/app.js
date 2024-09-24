235ef08dcdd49d79d36720dc3b5971bb5f8ec835
rmathena79 <unique1478@gmail.com>
2024-09-23 20:27:24 -0700

    Populating sample metadata

--- static/js/app.js
+++ static/js/app.js
@@ -1,27 +1,33 @@
 // Build the metadata panel
 function buildMetadata(sample) {
+  console.log(`Building metadata for sample ${sample}`);
   d3.json("https://static.bc-edx.com/data/dl-1-2/m14/lms/starter/samples.json").then((data) => {
 
     // get the metadata field
-
+    let allMetadata = data.metadata;
 
     // Filter the metadata for the object with the desired sample number
-
+    let sampleMetadata = allMetadata.filter(x => x.id == sample)[0]
 
     // Use d3 to select the panel with id of `#sample-metadata`
-
+    let panel = d3.selectAll("#sample-metadata");
 
     // Use `.html("") to clear any existing metadata
-
+    panel.html("");
 
     // Inside a loop, you will need to use d3 to append new
     // tags for each key-value in the filtered metadata.
-
+    Object.keys(sampleMetadata).forEach(key => {
+      let description = `${key}: ${sampleMetadata[key]}`;
+      console.log(description);
+      panel.append("p").text(description);
+    });
   });
 }
 
 // function to build both charts
 function buildCharts(sample) {
+  console.log(`Building charts for sample ${sample}`);
   d3.json("https://static.bc-edx.com/data/dl-1-2/m14/lms/starter/samples.json").then((data) => {
 
     // Get the samples field
@@ -56,39 +62,35 @@ function init() {
   d3.json("https://static.bc-edx.com/data/dl-1-2/m14/lms/starter/samples.json").then((data) => {
 
     // Get the names field
-    names = data.names;
+    let names = data.names;
     console.log(`names: ${names}`);
 
     // Use d3 to select the dropdown with id of `#selDataset`
-    dropdown = d3.selectAll("#selDataset")
+    let dropdown = d3.selectAll("#selDataset")
 
     // Use the list of sample names to populate the select options
     // Hint: Inside a loop, you will need to use d3 to append a new
     // option for each sample name.
-    let selector = d3.select("#selDataset");            
     names.forEach((name) => {
-        selector
-            .append("option")
-            .text(name)
-            .property("value", name);
+      dropdown.append("option").text(name).property("value", name);
     });      
       
     // Get the first sample from the list
-    samples = data.samples;
-    sample = samples[0];
-    console.log(sample);
+    let samples = data.samples;
+    let sampleID = samples[0].id;
 
     // Build charts and metadata panel with the first sample
-    buildCharts(sample);
-    buildMetadata(sample);
+    buildCharts(sampleID);
+    buildMetadata(sampleID);
   });
 }
 
 // Function for event listener
-function optionChanged(newSample) {
+function optionChanged(newSampleID) {
   // Build charts and metadata panel each time a new sample is selected
-  buildCharts(newSample);
-  buildMetadata(newSample);
+  console.log(`New sample: ${newSampleID}`);
+  buildCharts(newSampleID);
+  buildMetadata(newSampleID);
 }
 
 // Initialize the dashboard
